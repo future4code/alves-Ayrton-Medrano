84f2e47a8f0c38178a380b49e1b99a5f5226921f
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-06-22 16:57:33 -0300

    entrega

--- modulo2/apis-labenusers/labenusers/src/App.js
+++ modulo2/apis-labenusers/labenusers/src/App.js
@@ -1,21 +1,32 @@
+import React from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import PrimeiraTela from './components/PrimeiraTela'
+import SegundaTela from './components/SegundaTela'
 
-function App() {
-  return (
-    <div>
-      <h1>Cadastro de Usuários</h1>
-      <label for="nome">
-        <input type="text" placeholder="Digite seu nome" />
-      </label>
-      <label for="email">
-        <input type="text" placeholder="Digite seu e-mail" />
-      </label>
-      <button>Cadastre-se</button>
-      <br />
-      <button>Tela de descrição</button>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    telaAtual: 'cadastro'
+  }
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+      case 'cadastro':
+        return <PrimeiraTela irParaLista={this.irParaLista} />
+      case 'lista':
+        return <SegundaTela irParaCadastro={this.irParaCadastro} />
+      default:
+        return <div>Página não encontrada</div>
+    }
+  }
+  irParaCadastro = () => {
+    this.setState({ telaAtual: 'cadastro' })
+  }
+  irParaLista = () => {
+    this.setState({ telaAtual: 'lista' })
+  }
+
+  render() {
+    return <div>{this.escolheTela()}</div>
+  }
 }
-
 export default App
