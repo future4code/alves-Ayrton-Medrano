2dab10df751bf13c1aea468e679de7039e4b11f5
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-08-11 17:35:58 -0300

    Entrega quinta

---
+++ modulo4/aprofundamento-typescript/src/ex1.ts
@@ -0,0 +1,47 @@
+// Ex 1 -
+
+// a)
+// let minhaString :string = "Oito"
+// minhaString=8
+// -- A variável não aceita o tipo number por estar definida como uma string
+
+
+// b)
+  // let meuNumero: number = 10
+  // meuNumero= "Dez"
+// -- A variável não aceita o tipo string por estar definida como number
+// 
+// c)
+// type Person ={
+//   nome: string|null,
+//   idade: number|null,
+//   corFavorita: string|null,
+// }
+
+// let joaozinho : Person = {
+//   nome: "joao",
+//   idade: 18,
+//   corFavorita: CorFavorita.AZUL
+// }
+// let mariazinha : Person = {
+//   corFavorita: CorFavoria.VERDE
+//   idade: 18,
+//   nome:"Maria"
+
+// }
+// let joaquim : Person = {
+//   nome: "joaquim",
+//   idade: 18,
+//   corFavorita: CorFavorita.VERMELHO
+// }
+
+// d)
+//  enum CorFavorita {
+//   VERMELHO= "Vermelho",
+//   LARANJA= "Laranja",
+//   AMARELO= "Amarelo",
+//   VERDE= "Verde",
+//   AZUL= "Azul",
+//   ANIL= "Anil",
+//   VIOLETA= "Violeta"
+//  }
---
+++ modulo4/aprofundamento-typescript/src/ex4.ts
@@ -0,0 +1,30 @@
+// //4-
+// //a)
+// type pokemon = {
+// 	name: string,
+//   types: string,
+// 	healthPoints: number
+// }
+
+// const pokemon1: pokemon = {
+//   name: "Charmander",
+//   types: "Fire",
+//   healthPoints: 28
+// }
+
+// const pokemon2: pokemon = {
+//   name: "Bulbasaur",
+//   types: "Grass/Poison",
+//   healthPoints: 31
+// }
+
+// const pokemon3: pokemon = {
+//   name: "Squirtle",
+//   types: "Water",
+//   healthPoints: 35
+// }
+//b) No meu caso eu coloquei o tsc && antes do start no package.json e ao rodar o npm start já consigo transpilar para o js na pasta build
+
+//c) Acredito que não pois meu arquivo atual está dentro da pasta src e funcionou.
+
+//d) Não sei como faria 
