2190e15668d11de45f1708e3b2217f737f3cdb47
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-08-29 08:44:49 -0300

    final

--- modulo4/projeto-to-do/src/index.ts
+++ modulo4/projeto-to-do/src/index.ts
@@ -2,5 +2,27 @@ import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import app from './app'
 import createUser from './endpoints/createUser'
+import getUserById from './endpoints/getUserbyId'
+import editUser from './endpoints/editUser'
+import createTask from './endpoints/createTask'
+import getTaskById from './endpoints/getTaskById'
+import getAllUsers from './endpoints/getAllUsers'
 
+
+//1
 app.post('/user',createUser)
+
+//2
+app.get("/user/:id", getUserById)
+
+//3
+app.put("/user/edit/:id", editUser)
+
+//4
+app.post("/task", createTask)
+
+//5
+app.get("/task/:id", getTaskById)
+
+//6
+app.get("/user/all", getAllUsers)
